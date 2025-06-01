898d04dbc1d2923312ba26cb98505151ae872365
Kaique <kaiquesimoes58@gmail.com>
2025-06-01 19:48:58 -0300

    BugFix CORS

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.routers import UserRouters, TransactionRouters, GoalRouters, PiggyRouters
 from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -33,3 +34,16 @@ def custom_openapi():
     return app.openapi_schema
 
 app.openapi = custom_openapi
+
+origins = [
+    "https://easy-finance-ten.vercel.app",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
