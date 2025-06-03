a4f8743df1b1bbef34cdb4e367314bfc69842ef0
Kaique <kaiquesimoes58@gmail.com>
2025-06-03 16:09:46 -0300

    Bugfix Cookies

--- api/utils/auth.py
+++ api/utils/auth.py
@@ -2,12 +2,15 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from api.utils.config import SECRET_KEY, ALGORITHM
+from fastapi import Cookie
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-def get_user_id_from_token(token: str = Depends(oauth2_scheme)) -> int:
+def get_user_id_from_token(access_token: str = Cookie(None)):
+    if access_token is None:
+        raise HTTPException(status_code=401, detail="Não autenticado")
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
         return int(payload.get("sub"))
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido ou expirado")
